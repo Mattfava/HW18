0112f14cf7adebdb19f839711eb1b413535d1a15
Matt Fava <matt.b.fava@gmail.com>
2020-08-12 16:24:17 -0400

    changed port method

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -23,6 +23,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(port, () => {
+  console.log(`App running on port ${port}!`);
 });
